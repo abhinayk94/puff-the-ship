979f68f48021deb6bf063ac02530ccfc3dd207d3
Abhinay <abhinayk94@gmail.com>
2014-06-20 02:28:26 +0800

    adding entire project, for the first time

---
+++ Assets/transition.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class transition : MonoBehaviour {
+
+	// Use this for initialization
+	public double i;
+	void Start () {
+		while (i < 500009001) {
+			i=i+1;
+				}
+		Application.LoadLevel ("scene0");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
